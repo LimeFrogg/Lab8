0d8345a174df2d1f132b0ac26c6c9b3e1f0529aa
LimeFrogg <bellamones@icloud.com>
2024-11-14 10:50:42 -0500

    Question 6

--- script.js
+++ script.js
@@ -39,3 +39,15 @@ if (startValue = 0){
     console.log(startvalue);
 }
 
+var a = 10
+
+var b = 20
+
+console.log(a==b)
+console.log(a===b)
+console.log(a!=b)
+console.log(a>b)
+console.log(a<b)
+console.log(a>=b)
+console.log(a<=b)
+
