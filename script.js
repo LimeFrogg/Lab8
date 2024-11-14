1a121410512f57fac3ef930c7e2d05d2afd79843
LimeFrogg <bellamones@icloud.com>
2024-11-14 10:39:50 -0500

    Question 2

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 var Name = "Isabella"
 var Age = "18"
 console.log(Name + Age);
+
+var House;
+console.log(House);
+
+var isJavaEasy = false;
+console.log(isJavaEasy);
+
+const str = "A String"
+HTMLFormControlsCollection.log(str);
